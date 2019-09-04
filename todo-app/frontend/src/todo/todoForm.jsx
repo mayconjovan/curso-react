bc309e3243abce8f3fddd768b96147a1bcd96e1e
Maycon <mayconjovan@gmail.com>
2019-09-04 01:48:31 -0300

    Ajuste de layout

--- todo-app/frontend/src/todo/todoForm.jsx
+++ todo-app/frontend/src/todo/todoForm.jsx
@@ -11,12 +11,12 @@ export default props => (
                 value={props.description}></input>
         </Grid>
         <Grid cols='12 3 2'>
-            <IconButton style='primary' icon='plus' 
-            onClick={props.handleAdd}></IconButton>
+            <IconButton style='primary' icon='plus'
+                onClick={props.handleAdd}></IconButton>
             <IconButton style='info' icon='search'
-            onClick={props.handleSearch}></IconButton>
+                onClick={props.handleSearch}></IconButton>
             <IconButton style='default' icon='close'
-            onClick={props.handleClear}></IconButton>
+                onClick={props.handleClear}></IconButton>
         </Grid>
     </div>
 )
