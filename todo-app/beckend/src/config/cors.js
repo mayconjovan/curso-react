737a144f2a3a4765fd184b4504cc81d4f44f9b25
Maycon <mayconjovan@gmail.com>
2019-09-04 00:34:00 -0300

    Primeiro teste da aplicação

--- todo-app/beckend/src/config/cors.js
+++ todo-app/beckend/src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = function(req, res, next) {
-    res.header('Acces-Control-Allow-Origin', '*')
-    res.header('Acces-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-type, Accept')
     next()
 }
